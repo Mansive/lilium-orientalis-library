3d832a81ba006203cc027c5558e34af8875b59dc
Mansive <xmansive@gmail.com>
2023-12-18 04:56:41 -0600

    Transfer builds and deployments to Github

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'lolibrary',
-  description: 'TODO',
-}
+  title: "lolibrary",
+  description: "Search for Japanese literature.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
