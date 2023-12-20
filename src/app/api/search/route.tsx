b088846a64e34974e77539b5cea54d9dc632ea41
Mansive <xmansive@gmail.com>
2023-12-20 05:50:16 -0600

    Establish db connection and basic search

---
+++ src/app/api/search/route.tsx
@@ -0,0 +1,19 @@
+import { getXataClient } from "@/lib/xata";
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+const xata = getXataClient();
+
+export async function GET(request: NextRequest) {
+  const searchParams: string = decodeURIComponent(
+    request.nextUrl.searchParams.toString()
+  );
+
+  const results = await xata.db.Books.search(searchParams, {
+    target: ["title"],
+    fuzziness: 0,
+  });
+
+  return NextResponse.json({ results });
+}
