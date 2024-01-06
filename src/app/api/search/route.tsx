d5840e2ee7f69df5ccf7cfd43dee6d42405f6e66
Mansive <33560917+Mansive@users.noreply.github.com>
2024-01-06 05:47:10 -0600

    Reduce transfer size

--- src/app/api/search/route.tsx
+++ src/app/api/search/route.tsx
@@ -16,11 +16,26 @@ export async function GET(request: NextRequest) {
     );
   }
 
-  const results = await xata.db.Books.search(searchParams, {
-    target: ["title"],
-    fuzziness: 0,
-    page: { size: 50, offset: 0 },
-  });
+  try {
+    const results = await xata.db.Books.search(searchParams, {
+      target: ["title"],
+      fuzziness: 0,
+      page: { size: 50, offset: 0 },
+    });
 
-  return NextResponse.json(results);
+    // Pasted from Copilot; could probably be done better
+    const records = JSON.parse(JSON.stringify(results))["records"];
+    records.forEach((book: Record<string, any>) => {
+      delete book.id;
+      delete book.embeddings;
+      delete book.xata;
+    });
+
+    return NextResponse.json({ records });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "A strange error has ocurred" },
+      { status: 500 }
+    );
+  }
 }
