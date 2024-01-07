ae6084d2d6be147d219c777319010f88ec3bfd15
Mansive <33560917+Mansive@users.noreply.github.com>
2024-01-07 00:50:59 -0600

    Temporarily switch to vector search

--- src/app/api/search/route.tsx
+++ src/app/api/search/route.tsx
@@ -17,19 +17,42 @@ export async function GET(request: NextRequest) {
   }
 
   try {
-    const results = await xata.db.Books.search(searchParams, {
-      target: ["title"],
-      prefix: "phrase",
-      fuzziness: 0,
-      page: { size: 35, offset: 0 },
-    });
+    // const results = await xata.db.Books.search(searchParams, {
+    //   target: ["title"],
+    //   prefix: "phrase",
+    //   fuzziness: 0,
+    //   page: { size: 35, offset: 0 },
+    // });
 
-    // Pasted from Copilot; could probably be done better
+    // Generate embeddings
+    const embeddings = await fetch("https://api.embaas.io/v1/embeddings/", {
+      method: "POST",
+      body: JSON.stringify({
+        texts: ["query: " + searchParams],
+        model: "multilingual-e5-large",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.EMBAAS_API_KEY,
+      },
+    }).then((response) => response.json());
+
+    const results = await xata.db.Books.vectorSearch(
+      "embeddings",
+      embeddings["data"][0]["embedding"],
+      {
+        similarityFunction: "cosineSimilarity",
+        size: 35,
+      }
+    );
+
+    // Reduce network payload size for users
     const records = JSON.parse(JSON.stringify(results))["records"];
     records.forEach((book: Record<string, any>) => {
       delete book.id;
       delete book.embeddings;
-      delete book.xata;
+      //delete book.xata;
+      console.log(book);
     });
 
     return NextResponse.json({ records });
